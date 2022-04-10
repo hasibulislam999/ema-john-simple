7916213c0416b4f489cf92c7c3d3810710d2d155
hasibulislam999 <hasib143sl@gmail.com>
2022-04-10 22:22:31 +0600

    protect expected pages

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Login.css';
 import auth from '../../firebase.init';
 import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
@@ -9,6 +9,8 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
     const handleInputEmail = (event) => {
         setEmail(event.target.value);
@@ -20,7 +22,7 @@ const Login = () => {
     };
 
     if (user) {
-        navigate('/orders');
+        navigate(from, {replace: true});
     }
 
     const submitForm = (event) => {
